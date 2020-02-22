4ff2ee2bddaada17078307ba52747e040d59afe2
Oscar Diaz <odiaz.dw@gmail.com>
2020-02-22 15:08:36 -0600

    model smoothness shaking

--- js/interaction.js
+++ js/interaction.js
@@ -3,15 +3,21 @@ let state = {
   arrowsIndex: 0,
   direction: '',
   hasClicked: false,
-  modelPositions: [
-    -0.8,
-    -0.6,
-    -0.4,
-    -0.2
+  showCasePos: {
+    x: 1.75,
+    y: 2,
+    z: 9
+  },
+  cubesInitialPos: [
+    {x: 0, y: 0, z: 0.065},
+    {x: -0.013, y: 0, z: 0.065},
+    {x: -0.00784, y: 0, z: 0.065},
+    {x: -0.06673, y: 0, z: 0.065},
   ]
 };
 
 let spotLight = null;
+let cubes = [];
 let cubeLights = null;
 
 export const registerListeners = () => {
@@ -46,15 +52,34 @@ const handleArrowControls = direction => {
 const resolveLightPosition = (direction) => {
   state.hasClicked = true;
   state.direction = direction;
+
   cubeLights = document.querySelectorAll('a-entity[light]');
+
   cubeLights.forEach(light => {
     light.setAttribute('visible', false);
   })
   cubeLights[state.arrowsIndex].setAttribute('visible', true);
+
+  cubes.forEach((cube, index) => {
+    cube.setAttribute('position', state.cubesInitialPos[index]);
+  })
 }
 
 AFRAME.registerComponent("awake", {
   init: function() {
-    spotLight = document.querySelector('#spot-light');
+    spotLight = document.querySelector("#spot-light");
+    cubes = document.querySelectorAll('a-entity[cube]');
+  },
+  tick: function() {
+    if (cubes[state.arrowsIndex].object3D.position.z === state.showCasePos.z) {
+      state.hasClicked = false
+    }
+    if (state.hasClicked) {
+      cubes[state.arrowsIndex].object3D.position.lerp({
+        x: state.showCasePos.x - (state.arrowsIndex * 1.2),
+        y: state.showCasePos.y,
+        z: state.showCasePos.z
+      }, 0.1);
+    }
   }
 });
