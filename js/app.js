6b629a80f8e3117240696094d90c46ff130b9ade
Oscar Diaz <odiaz.dw@gmail.com>
2020-02-22 15:16:54 -0600

    model smoothness shaking

--- js/app.js
+++ js/app.js
@@ -5,6 +5,12 @@ var stateInterval = null;
 function init () {
   hideDebugUI();
   Interaction.registerListeners();
+  // Video
+  const videoEl = document.getElementById("oro-intro");
+  const random = Math.floor(Math.random() * 100);
+  videoEl.setAttribute("src", `/assets/videos/oro-intro.mp4?random=${random}`);
+  videoEl.setAttribute('id', 'oro-intro');
+  videoEl.setAttribute('autoplay', 'true');
 }
 
 function hideDebugUI() {
